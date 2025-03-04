435560f69607dffcc32d9dea52669ae0a18c3037
lihuikun <lihuikun@wezonet.com>
2025-03-04 18:39:03 +0800

    feat: 更新环境变量，添加 Pinia 和持久化插件，调整 Tailwind CSS 配置以支持暗模式

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,25 @@ export default {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // 定义亮色模式和暗色模式下的颜色
+        gray: {
+          50: 'var(--color-gray-50)',
+        },
+        white: 'var(--color-bg-white)',
+      },
+      textColor:{
+        white: 'var(--color-text-white)',
+      }
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['dark'], // 启用 dark 模式下的背景颜色变体
+    },
   },
   plugins: [],
 }
